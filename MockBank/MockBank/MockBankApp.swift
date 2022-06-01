acd2b9a2333881f92340e0a18195bb1f8a048143
Nuno Mota <nuno.mota@inetum.com>
2022-06-01 19:28:11 +0100

    create project

---
+++ MockBank/MockBank/MockBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockBankApp.swift
+//  MockBank
+//
+//  Created by Nuno Mota on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MockBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
