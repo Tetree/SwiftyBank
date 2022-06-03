40e47e293b336fca7a1c4adc6bc6c98741db3027
Nuno Mota <nuno.mota@inetum.com>
2022-06-03 23:34:02 +0100

    Add SwiftUIFontIcon

--- MockBank/MockBank/Views/TransactionRow.swift
+++ MockBank/MockBank/Views/TransactionRow.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct TransactionRow: View {
     let transaction: Transaction
@@ -16,6 +17,9 @@ struct TransactionRow: View {
             RoundedRectangle(cornerRadius: 20, style: .continuous)
                 .fill(Color.icon.opacity(0.3))
                 .frame(width: 44, height: 44)
+                .overlay {
+                    FontIcon.text(.awesome5Solid(code: .icons), fontsize: 24, color: Color.icon)
+                }
             
             VStack(alignment: .leading, spacing: 6) {
                 Text(transaction.merchant)
